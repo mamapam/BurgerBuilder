6f4258de2676abb1d9078910dc1d8e331f0c7e4b
mamapam <mamapam.dev@gmail.com>
2020-06-07 18:41:14 -0400

    Connected state to build controls and added burger price

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,15 +4,17 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  let transformedIngredients = Object.keys(props.ingredients)
-    .map(igKey => {
-      return [...Array(props.ingredients[igKey])].map((_, index) => {
-        return <BurgerIngredient key={igKey + index} type={igKey} />
-      });
-    })
-    .reduce((arr, el) => {
-      return arr.concat(el);
-    }, []);
+  let transformedIngredients = Object.keys(props.ingredients) // [salad, bacon, cheese, meat]
+  .map(igKey => {
+    // create array of keys repeating each time for the quantity required
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey+i} type={igKey} />
+    }); 
+  })
+  // flatten array of arrays
+  .reduce((acc, el) => {
+    return acc.concat(el);
+  }, []); 
   
   if(transformedIngredients.length === 0) {
     transformedIngredients = <p>Please start adding ingredients!</p>
