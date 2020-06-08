f54e1da2574f755bc607303f1224684f55f333a8
mamapam <mamapam.dev@gmail.com>
2020-06-07 22:09:47 -0400

    Performance update of checking if modal should be updated

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxiliary from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+  // This could be functional component, doesn't have to be class
+  componentDidUpdate() {
+    console.log('[OrderSummary] Will Update');
+  }
+
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
       return (
         <li key={igKey}>
-          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
         </li>
       );
     });
 
-  return(
-    <Auxiliary>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinue}>CONTINUE</Button>
-    </Auxiliary>
-  );
-};
+    return(
+      <Auxiliary>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinue}>CONTINUE</Button>
+      </Auxiliary>
+    );
+  } 
+}
 
-export default orderSummary;
+export default OrderSummary;
