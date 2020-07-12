f88ce396ec1460deee4eb570a517e1156a172dd9
mamapam <mamapam.dev@gmail.com>
2020-07-12 13:40:54 -0400

    Redirect unauthenticated user to login on order button

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import  { connect } from 'react-redux'
-;
+import  { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
 import classes from './Auth.module.css';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
@@ -113,8 +114,14 @@ class Auth extends Component {
       );
     }
 
+    let authRedirect = null;
+    if(this.props.isAuthenticated) {
+      authRedirect = <Redirect to="/" />
+    }
+
     return(
       <div className={classes.Auth}>
+        {authRedirect}
         {errorMessage}
         <form onSubmit={this.submitHandler}>
           {form}
@@ -131,7 +138,8 @@ class Auth extends Component {
 const mapStateToProps = state => {
   return {
     loading: state.auth.loading,
-    error: state.auth.error
+    error: state.auth.error,
+    isAuthenticated: state.auth.token !== null
   }
 };
 
