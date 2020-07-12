26fd0c6bc4afb6e6579cef562ab7ff23b6083571
mamapam <mamapam.dev@gmail.com>
2020-07-12 16:47:03 -0400

    Add local storage

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,9 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('expirationTime');
+  localStorage.removeItem('userId');
   return {
     type: actionTypes.AUTH_LOGOUT
   };
@@ -51,6 +54,10 @@ export const auth = (email, password, isSignup) => {
     axios.post(url, authData)
       .then(response => {
         console.log(response);
+        const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+        localStorage.setItem('token', response.data.idToken);
+        localStorage.setItem('expirationTime', expirationDate);
+        localStorage.setItem('userId', response.data.localId);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
         dispatch(checkAuthTimeout(response.data.expiresIn));
       })
@@ -66,4 +73,22 @@ export const setAuthRedirectPath = (path) => {
     type: actionTypes.SET_AUTH_REDIRECT_PATH,
     path: path
   }
+};
+
+export const authCheckState = () => {
+  return dispatch => {
+    const token = localStorage.getItem('token');
+    if(!token) {
+      dispatch(logout());
+    } else {
+      const expirationTime = new Date(localStorage.getItem('expirationTime'));
+      if(expirationTime <= new Date()) {
+        dispatch(logout());
+      } else {
+        const userId = localStorage.getItem('userId');
+        dispatch(authSuccess(token, userId));
+        dispatch(checkAuthTimeout((expirationTime.getTime() - new Date().getTime()) / 1000));
+      }
+    }
+  };
 };
